5c453a0d4cf31fcc21d8278490c8ec4d661746ad
SathvikHegde <62826093+SathvikHegde@users.noreply.github.com>
2021-08-18 11:04:58 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const cleverbot = require("cleverbot-free");
+
+let precommand;
+let preresponse;
+
+client.once('ready', () => {
+    console.log('Bot is online!');
+});
+
+client.on('message', message => {
+    if(message.channel.id != '877421615493828628') return;
+
+    if(message.content == '-start') {
+        return client.channels.cache.get('877421615493828628').send('Hi!');
+    }
+
+    cleverbot(message.content, [precommand, preresponse]).then(response =>{
+        message.channel.send(response);
+        precommand = message.content;
+        preresponse = response;
+    }); 
+});
+
+client.login(process.env.TOKEN);
